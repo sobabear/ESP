4d1a39ecd05e1cdce654f90a55a3efdfc43aa78a
Yongjun Lee <stareta1202@gmail.com>
2021-04-08 18:40:48 +0900

    initial project

---
+++ 01.basic/LEd/LEd.ino
@@ -0,0 +1,18 @@
+#define LED 2 // ESP 보드에 내장된 LED 핀은 2
+void setup() {
+  // put your setup code here, to run once:
+
+
+  Serial.begin(115200);
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  delay(500);
+  digitalWrite(LED,HIGH);
+  delay(1500);
+  digitalWrite(LED,LOW);
+  Serial.println("working");
+  // put your main code here, to run repeatedly:
+
+}
