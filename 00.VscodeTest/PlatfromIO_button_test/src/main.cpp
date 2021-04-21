3aecc01fffee81bcd4b903119b524d74c373a5d7
Yongjun Lee <stareta1202@gmail.com>
2021-04-21 18:27:07 +0900

    esp-idf 추가

---
+++ 00.VscodeTest/PlatfromIO_button_test/src/main.cpp
@@ -0,0 +1,24 @@
+#include"Arduino.h"
+#define LED 2 // ESP 보드에 내장된 LED 핀은 2
+ int i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+  Serial.begin(115200);
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  delay(500);
+  digitalWrite(LED,HIGH);
+        delay(1500);
+  digitalWrite(LED,LOW);
+  Serial.print(i);
+  Serial.println("working");
+  i++;
+
+  // put your main code here, to run repeatedly:
+
+}
--- 01.basic/button/button.ino
+++ 01.basic/button/button.ino
@@ -1,3 +1,4 @@
+
 #define TAREBTN 2
 
 
